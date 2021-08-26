2caa4d2aa6ae5235ba9368da7f512b44ae53ed1c
ManApart <r@austinkucera.com>
2021-08-25 20:59:42 -0400

    attempt to calculate line height

--- src/commonMain/kotlin/main.kt
+++ src/commonMain/kotlin/main.kt
@@ -5,30 +5,26 @@ import com.soywiz.korge.input.keys
 import com.soywiz.korge.ui.*
 import com.soywiz.korge.view.alignBottomToBottomOf
 import com.soywiz.korge.view.alignBottomToTopOf
+import com.soywiz.korge.view.positionY
 import com.soywiz.korim.color.Colors
 import com.soywiz.korim.text.TextAlignment
 
 const val WIDTH = 512.0
 const val HEIGHT = 512.0
+const val LINE_HEIGHT = 15.0
 
 @KorgeExperimental
 suspend fun main() = Korge(width = WIDTH.toInt(), height = HEIGHT.toInt(), bgcolor = Colors["#2b2b2b"]) {
     val completer = StringCompleter("apple", "bob", "candy")
     val processor = ExampleProcessor()
-    val history = (0..20).map { "test $it" }.toMutableList()
+//    val history = (0..20).map { "test $it" }.toMutableList()
+    lateinit var history: UIText
 
-//    lateinit var history: UIText
-//    uiScrollable(width = WIDTH, height = HEIGHT - 40.0) {
-//        history = uiText("test\nline2\nline3".repeat(10), WIDTH, HEIGHT - 40.0) {
-//            textAlignment = TextAlignment.TOP_LEFT
+//    val stack = uiVerticalStack(WIDTH) {
+//        history.forEach {
+//            uiText(it, WIDTH, 20.0)
 //        }
 //    }
-
-    val stack = uiVerticalStack(WIDTH) {
-        history.forEach {
-            uiText(it, WIDTH, 20.0)
-        }
-    }
 
     val suggestions = uiText("", WIDTH) {
         alignBottomToBottomOf(parent!!)
@@ -38,6 +34,16 @@ suspend fun main() = Korge(width = WIDTH.toInt(), height = HEIGHT.toInt(), bgcol
         alignBottomToTopOf(suggestions)
         focus()
     }
+
+//    uiScrollable(width = WIDTH, height = HEIGHT - 40.0) {
+        val text = (0..60).joinToString("\n") { "test $it" }
+        val offset = -LINE_HEIGHT * text.count { it == '\n' }
+        history = uiText(text, WIDTH, HEIGHT-40) {
+            textAlignment = TextAlignment.TOP_LEFT
+            alignBottomToTopOf(prompt)
+            positionY(offset)
+        }
+//    }
 
     keys {
         up(Key.TAB) {
@@ -55,16 +61,17 @@ suspend fun main() = Korge(width = WIDTH.toInt(), height = HEIGHT.toInt(), bgcol
         }
         up(Key.ENTER) {
             val text = prompt.text
-//            history.text += processor.process(text) + "\n"
-            history.subList(1, history.size).forEachIndexed { i, line ->
-                history[i] = line
-            }
-            history[history.size - 1] = processor.process(text)
-            (0 until history.size).forEach { i ->
-                (stack[i] as UIText).text = history[i]
-            }
-//            (stack[historyIndex] as UIText).text = processor.process(text)
-//            stack.addChild(uiText( processor.process(text), WIDTH, HEIGHT - 40.0))
+            history.text += processor.process(text) + "\n"
+            val offset = -LINE_HEIGHT * history.text.count { it == '\n' }
+            history.positionY(offset)
+//            history.height = offset.toDouble()
+//            history.subList(1, history.size).forEachIndexed { i, line ->
+//                history[i] = line
+//            }
+//            history[history.size - 1] = processor.process(text)
+//            (0 until history.size).forEach { i ->
+//                (stack[i] as UIText).text = history[i]
+//            }
             prompt.text = ""
         }
     }
